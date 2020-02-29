a543b78e70335a927d047ea32a4c1ed409f5e385
Rob Mayoff <git@rob.dqd.com>
2020-02-29 11:52:16 -0600

    work around ReactiveX/RxSwift#2125
    https://github.com/ReactiveX/RxSwift/issues/2125
    
    macOS prior to 10.12 does not support weak references to NSTextViews,
    and Xcode 11.4 beta enforces this as a compile-time error if the build
    target's deployment target is earlier than 10.12. RxCocoa 5.0.1's
    deployment target is 10.10.
    
    To work around the problem, I've marked this class, and the methods that
    use it, as only available on 10.12 or later. However, that's not enough
    to convince the compiler! So I've also changed the type of the weak
    reference to AnyObject?, and hidden the AnyObject? reference behind a
    computed property.

--- RxCocoa/macOS/NSTextView+Rx.swift
+++ RxCocoa/macOS/NSTextView+Rx.swift
@@ -14,16 +14,35 @@ import RxSwift
 /// Delegate proxy for `NSTextView`.
 ///
 /// For more information take a look at `DelegateProxyType`.
+@available(macOS 10.12, *)
 open class RxTextViewDelegateProxy: DelegateProxy<NSTextView, NSTextViewDelegate>, DelegateProxyType, NSTextViewDelegate {
 
+    /*
+     * macOS prior to 10.12 does not support weak references to
+     * NSTextViews, and Xcode 11.4 beta enforces this as a compile-time
+     * error if the build target's deployment target is earlier than
+     * 10.12. RxCocoa 5.0.1's deployment target is 10.10.
+     *
+     * To work around the problem, I've marked this class, and the
+     * methods that use it, as only available on 10.12 or later.
+     * However, that's not enough to convince the compiler! So I've also
+     * changed the type of the weak reference to AnyObject?, and hidden
+     * the AnyObject? reference behind a computed property.
+     */
+
     /// Typed parent object.
-    public weak private(set) var textView: NSTextView?
+    public private(set) var textView: NSTextView? {
+        get { return unsafeBitCast(_textView, to: NSTextView?.self) }
+        set { _textView = newValue }
+    }
+
+    private weak var _textView: AnyObject?
 
     /// Initializes `RxTextViewDelegateProxy`
     ///
     /// - parameter textView: Parent object for delegate proxy.
     init(textView: NSTextView) {
-        self.textView = textView
+        self._textView = textView
         super.init(parentObject: textView, delegateProxy: RxTextViewDelegateProxy.self)
     }
 
@@ -61,11 +80,13 @@ extension Reactive where Base: NSTextView {
     /// Reactive wrapper for `delegate`.
     ///
     /// For more information take a look at `DelegateProxyType` protocol documentation.
+    @available(macOS 10.12, *)
     public var delegate: DelegateProxy<NSTextView, NSTextViewDelegate> {
         return RxTextViewDelegateProxy.proxy(for: self.base)
     }
 
     /// Reactive wrapper for `string` property.
+    @available(macOS 10.12, *)
     public var string: ControlProperty<String> {
         let delegate = RxTextViewDelegateProxy.proxy(for: self.base)
 
